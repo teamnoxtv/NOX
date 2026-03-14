a5282c2bb964a28bfb8714af476b1ab7747210be
teamnoxtv <noxoffically@gmail.com>
2026-03-14 11:58:44 +0000

    Update particles.js

--- scripts/particles.js
+++ scripts/particles.js
@@ -1,1 +1,25 @@
+particlesJS("particles-js", {
 
+particles:{
+
+number:{value:80},
+
+color:{value:"#a020f0"},
+
+shape:{type:"circle"},
+
+opacity:{value:0.6},
+
+size:{value:3},
+
+line_linked:{
+enable:true,
+distance:150,
+color:"#a020f0"
+},
+
+move:{speed:2}
+
+}
+
+})
