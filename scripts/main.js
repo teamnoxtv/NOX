fbe0eef60a0d85fa9eb081a6797098687ea821af
teamnoxtv <noxoffically@gmail.com>
2026-03-14 15:26:18 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -69,3 +69,15 @@ document.title = originalTitle
 }
 
 })
+<script>
+
+const cursor = document.querySelector(".cursor")
+
+document.addEventListener("mousemove", e => {
+
+cursor.style.left = e.clientX + "px"
+cursor.style.top = e.clientY + "px"
+
+})
+
+</script>
