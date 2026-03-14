9dfb0ce553fe189819047a5e07abd145bdf28917
teamnoxtv <noxoffically@gmail.com>
2026-03-14 11:59:40 +0000

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,1 +1,16 @@
 
+function uploadClip(){
+
+let title = document.getElementById("title").value
+let player = document.getElementById("player").value
+let url = document.getElementById("url").value
+
+let clips = JSON.parse(localStorage.getItem("noxClips")) || []
+
+clips.push({title,player,url})
+
+localStorage.setItem("noxClips",JSON.stringify(clips))
+
+alert("Clip Uploaded")
+
+}
