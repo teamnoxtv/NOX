f35bf0886783fb0975416d2dde4c8a2bcb6ae73a
teamnoxtv <noxoffically@gmail.com>
2026-03-14 11:58:31 +0000

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,25 @@
 
+/* STAT COUNTER ANIMATION */
+
+function counter(id,target){
+
+let element = document.getElementById(id)
+
+let count = 0
+
+let interval = setInterval(()=>{
+
+count++
+
+element.innerText = count
+
+if(count >= target) clearInterval(interval)
+
+},30)
+
+}
+
+counter("wins",12)
+counter("players",8)
+counter("clips",54)
+counter("community",1200)
