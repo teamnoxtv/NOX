94fe44c6ceaa7ce8d49184065a65c000ec4ce9f8
teamnoxtv <noxoffically@gmail.com>
2026-03-14 11:59:17 +0000

    Update clips.js

--- scripts/clips.js
+++ scripts/clips.js
@@ -1,1 +1,34 @@
 
+function loadClips(){
+
+let clips = JSON.parse(localStorage.getItem("noxClips")) || []
+
+let gallery = document.getElementById("clipGallery")
+
+gallery.innerHTML=""
+
+clips.forEach((clip,i)=>{
+
+let card = document.createElement("div")
+
+card.className="panel"
+
+card.innerHTML = `
+
+<h3>${clip.title}</h3>
+
+<p>${clip.player}</p>
+
+<video width="250" controls>
+<source src="${clip.url}">
+</video>
+
+`
+
+gallery.appendChild(card)
+
+})
+
+}
+
+loadClips()
